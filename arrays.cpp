c7b1e3f31b1b4b265af8def0896e75f24b4a4d59
mmubashirdev <muhammadmubashirramzan886@gmail.com>
2026-07-07 23:54:01 +0500

    arrays in cpp

---
+++ arrays.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int arr1[5];
+  
+  cout<<"Enter 5 elements in the array: "<<endl;
+  for (int i=0;i<5;i++){
+    cin>>arr1[i];
+  }
+  cout<<"The elements of array 1 are: ";
+  for(int i=0;i<5;i++){
+    cout<<arr1[i]<<" ";
+  }
+  cout<<endl;
+
+  return 0;
+}
