2d4aae13bb20f31facb5bd7ed6c1e5603c2ffa9c
mmubashirdev <muhammadmubashirramzan886@gmail.com>
2026-07-14 23:49:37 +0500

    function arg length

--- arrays.cpp
+++ arrays.cpp
@@ -1,17 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
   int arr1[5];
-  
-  cout<<"Enter 5 elements in the array: "<<endl;
-  for (int i=0;i<5;i++){
-    cin>>arr1[i];
-  }
-  cout<<"The elements of array 1 are: ";
-  for(int i=0;i<5;i++){
-    cout<<arr1[i]<<" ";
-  }
-  cout<<endl;
 
+  cout << "Enter 5 elements in the array: " << endl;
+  for (int i = 0; i < 5; i++)
+  {
+    cin >> arr1[i];
+  }
+  cout << "The elements of array 1 are: ";
+  for (int i = 0; i < 5; i++)
+  {
+    cout << arr1[i] << " ";
+  }
+  cout << endl;
   return 0;
 }
